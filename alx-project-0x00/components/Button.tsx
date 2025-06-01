b7a7d18af070f51f371b2c383d0fbaa8ca6e6f88
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-01 08:43:50 +0000

    used "className" instead of "styles" in the ButtonProps.. updated it

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,9 +1,9 @@
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({title, className=""
+const Button: React.FC<ButtonProps> = ({title, styles=""
 }) => {
   return (
-    <button className={`border px-3 py-1 text-lg mt-3 bg-blue-500 text-white ${className}`}>
+    <button className={`border px-3 py-1 text-lg mt-3 bg-blue-500 text-white ${styles}`}>
       {title}
     </button>
   );
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,19 +6,19 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button title="Small - sm" className="text-sm rounded-sm" />
-      <Button title="Small - md" className="text-sm rounded-md" />
-      <Button title="Small - full" className="text-sm rounded-full" />
+      <Button title="Small - sm" styles="text-sm rounded-sm" />
+      <Button title="Small - md" styles="text-sm rounded-md" />
+      <Button title="Small - full" styles="text-sm rounded-full" />
 
       <Card />
-      <Button title="Medium - sm" className="text-base rounded-sm" />
-      <Button title="Medium - md" className="text-base rounded-md" />
-      <Button title="Medium - full" className="text-base rounded-full" />
+      <Button title="Medium - sm" styles="text-base rounded-sm" />
+      <Button title="Medium - md" styles="text-base rounded-md" />
+      <Button title="Medium - full" styles="text-base rounded-full" />
 
       <Card />
-      <Button title="Large - sm" className="text-lg px-6 py-3 rounded-sm" />
-      <Button title="Large - md" className="text-lg px-6 py-3 rounded-md" />
-      <Button title="Large - full" className="text-lg px-6 py-3 rounded-full" />
+      <Button title="Large - sm" styles="text-lg px-6 py-3 rounded-sm" />
+      <Button title="Large - md" styles="text-lg px-6 py-3 rounded-md" />
+      <Button title="Large - full" styles="text-lg px-6 py-3 rounded-full" />
 
       <Card />
     </div>
